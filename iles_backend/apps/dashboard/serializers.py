b1a658c5f3c017070efe83820b275718bbfc1158
mmark6 <mmukisa64@gmail.com>
2026-04-10 20:07:02 +0300

    Remove unnecessary blank line in serializers.py

--- iles_backend/apps/dashboard/serializers.py
+++ iles_backend/apps/dashboard/serializers.py
@@ -6,7 +6,6 @@ class DashboardSummarySerializer(serializers.Serializer):
     total_logs = serializers.IntegerField()
     draft_logs = serializers.IntegerField()
     submitted_logs = serializers.IntegerField()
-    
     reviewed_logs = serializers.IntegerField()
     approved_logs = serializers.IntegerField()
     total_evaluations = serializers.IntegerField()
