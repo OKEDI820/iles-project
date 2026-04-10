e021f8614fc045c175db1049731a07c454981551
mmark6 <mmukisa64@gmail.com>
2026-04-10 21:06:37 +0300

    Update permissions.py

--- iles_backend/apps/common/permissions.py
+++ iles_backend/apps/common/permissions.py
@@ -6,7 +6,7 @@ class IsCoordinator(BasePermission):
         return bool(request.user and request.user.is_authenticated and request.user.role == ROLE_COORDINATOR)
 
 class IsSupervisor(BasePermission):
-    def has_permission(self, request):
+    def has_permission(self, request,view):
         return bool(request.user and request.user.is_authenticated and request.user.role == ROLE_SUPERVISOR)
 
 class IsStudent(BasePermission):
